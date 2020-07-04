23448735d90cd3f211b5cacd565b4724b54fc392
DESKTOP-15JEVLE\Vamsi kalyan <vamsi.flex@gmail.com>
2020-07-04 08:36:02 +0530

    added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ArrowMeetingComponent} from "./arrow-meeting/arrow-meeting.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: ArrowMeetingComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
